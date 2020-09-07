1c95e53190285dda31147c34f73a68d9fa435ed2
nhlengineer <nhlengineer@gmail.com>
2020-09-07 11:38:27 -0400

    tutorial21
    Caleb_C

--- numericdatatypes.c
+++ numericdatatypes.c
@@ -1,7 +1,23 @@
  /*Sal Morreale
-        8.29.2020
+        9.7.2020
         Caleb Curry youTube C programming tutorial 21*/
 		
 # include <stdio.h>
 
 int main()
+
+{
+        //types of varibles - int double float
+        int dogs = 6;
+
+        //floating point
+        //percision -- how much data the variable can hold
+
+        printf("%i %f %f/n", 1, 1111.1111, 1111.1111F); //"F" specifically calls a float;s
+        //passing in a float becomes a double
+        //doubles take up twice as much space
+
+        return 0;
+
+        
+}
